18f9352568dc3f59e921c7fbd7d81051036e88ce
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-14 13:49:39 -1000

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Capitalized first letters of firstName and lastName
+        // made the rest of the string lower case, incase the user typed it all caps
+        firstName = char.ToUpper(firstName[0]) + firstName.Substring(1).ToLower();
+        lastName = char.ToUpper(lastName[0]) + lastName.Substring(1).ToLower();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
