20cfd5f2020636a2af3728a1825fe9b343854a96
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-16 12:47:12 -1000

    completed step 1 of core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        // Console.WriteLine(number);
+
+        Console.Write("Guess a number from 1 to 100: ");
+        string guessed = Console.ReadLine();
+        int guess = int.Parse(guessed);
+
+        if (guess > number)
+        {
+            Console.WriteLine("Lower!");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else
+        {
+            Console.WriteLine($"You guessed it! The magic number is {number}");
+        }
     }
 }
