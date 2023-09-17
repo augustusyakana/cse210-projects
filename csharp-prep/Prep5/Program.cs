c0e8844438d42d8a3b768fe0677aab7298c9e743
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-16 18:57:45 -1000

    complete assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squared = SquareNumber(userNumber);
+        DisplayResult(squared, userName);
+
     }
+
+    static string DisplayWelcome()
+    {
+        string welcome = "Welcome to the Program!";
+        return welcome;
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number; 
+        return square;
+    }
+
+    static void DisplayResult(int number, string name)
+    {
+        name = char.ToUpper(name[0]) + name.Substring(1).ToLower(); // Capitalize first letter of name
+        Console.WriteLine($"{name}, the square of your favorite number is {number}.");
+    }
+
 }
