66006a7c83bc153ad5e09660fd3239cd71b05c24
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-10-12 19:02:36 -1000

    completed assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction();
+
+        // Test with 1
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        
+        // Test with 5
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        // Test with 3/4
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        // Test with 1/3 but using input
+        int top = 0;
+        int bottom = 0;
+
+        Console.Write("Enter numerator: ");
+        string num = Console.ReadLine();
+        Console.Write("Enter denominator: ");
+        string den = Console.ReadLine();
+
+        top = int.Parse(num);
+        bottom = int.Parse(den);
+
+        fraction4.SetTop(top);
+        fraction4.SetBottom(bottom);
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+
+        
+
     }
 }
