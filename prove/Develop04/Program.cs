e668a6a7c72dc519aa6916de615a07561f153ec0
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-10-29 19:17:58 -1000

    added menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the mindfulness program: ");
+        int response = 0;
+
+        while (response != 4) 
+        {
+
+            Console.WriteLine("Choose an activity below: ");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Your choice: ");
+            string choice = Console.ReadLine();
+            response = int.Parse(choice);
+
+            if (response == 1) 
+            {
+                Console.WriteLine("You chose the breathing activity");
+            } else if (response == 2) 
+            {
+                Console.WriteLine("You chose the Reflection Activity");
+            } else if (response == 3)
+            {
+                Console.WriteLine("You chose the Listing Activity");
+            } else if (response == 4) 
+            {
+                Console.WriteLine("Thank you!");
+                break;
+            } else 
+            {
+                Console.WriteLine("Invalid input! Please choose from 1 - 4 as shown in the menu.");
+            }
+        }
+
+        
+
+        
     }
 }
